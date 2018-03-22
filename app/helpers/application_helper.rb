3f45117d0caf0c7df6817eace93080d1209c8768
ryo arata <arata@v-force.co.jp>
2018-03-22 19:14:53 +0900

    開業を入れる

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,9 @@ module ApplicationHelper
 	  date_format = "%Y%m%d"
 	  (Date.today.strftime(date_format).to_i - birthday.strftime(date_format).to_i) / 10000
 	end
+
+	def simple_format_v1(text)
+	    return if text.blank?
+	    return text.gsub(/\r\n|\r|\n/, "<br />").html_safe
+	  end
 end
